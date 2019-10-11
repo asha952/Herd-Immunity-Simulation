a427fac1a09c3df8bb950801edb07cbaac6832e5
Cao Mai <caomai@rocketmail.com>
2019-10-10 18:28:53 -0700

    Added one more test funtion

--- test_simulation.py
+++ test_simulation.py
@@ -1,6 +1,7 @@
 from simulation import Simulation
 from virus import Virus
 from person import Person
+from logger import Logger
 
 def test_constructor():
     virus = Virus("HIV", 0.24, 0.12)
@@ -10,7 +11,13 @@ def test_constructor():
     assert sim.virus == virus
     assert sim.initial_infected == 2
     assert sim.vacc_percentage == .4
-
+    assert sim.next_person_id == 11
+    assert sim.total_infected == 0
+    assert sim.current_infected == 0
+    assert sim.total_infected == 0
+    assert sim.newly_infected == []
+    assert sim.save_from_vac == 0
+    assert sim.file_name == "_virus_name_HIV_simulation_pop_10_vp_0.4_infected_2.txt"
 
 def test_simulation_should_continue():
     virus = Virus("HIV", 0.24, 0.12)
@@ -26,8 +33,22 @@ def test_infect_newly():
     sim._infect_newly_infected()
     assert sim.total_infected == 0
 
-def test_time_step():
-    virus = Virus("HIV", 0.24, 0.12)
-    sim = Simulation(10, .4, virus, 2)
 
-    assert 
+def test_create_population():
+    virus = Virus("HIV", 0.50, 0.50)
+    sim = Simulation(10, .50, virus, 2)
+
+    vaccination_count = 0
+    infection_count = 0
+
+    for person in sim.population:
+        assert person.is_alive == True
+
+        if person.is_vaccinated:
+            vaccination_count += 1
+        if person.infection is not None:
+            infection_count += 1
+
+    assert vaccination_count == 5
+    assert infection_count == 2
+    assert len(sim.population) == 10
